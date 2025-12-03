8d390222af03cf1fec2cd975588f1f13cd7e4410
aiwen <aiwen@MacBook-de-aiwen.local>
2025-12-03 12:13:30 +0200

    rack cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000" # Next.js
+    origins "http://localhost:3000", "https://jombo.es", "https://www.jombo.es", "https://jombo.fi", "https://www.jombo.fi" # Next.js, Production
     resource "*",
       headers: :any,
       expose: ["Authorization"],
