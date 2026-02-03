364b9ffeac9088c9baca86b0c6b550ebbb38b4be
Albert Oliva <albert.oliva@gmail.com>
2026-02-03 13:16:19 +0200

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_secure_password validations: false
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :password, presence: true, if: :password_required?
   validates :language, presence: true, inclusion: { in: %w[en es fi] }
   
