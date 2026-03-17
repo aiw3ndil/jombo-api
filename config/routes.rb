cb329176707ff94a08d69bfb6e1f32a1daf968d5
Albert Oliva <albert.oliva@gmail.com>
2026-03-17 20:50:38 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       resources :trips do
         collection do
           get 'search', to: 'trips#search', as: 'search'
+          get 'search/:departure_location', to: 'trips#search'
           get ':departure_location-:arrival_location', to: 'trips#search'
           get 'my_trips', to: 'trips#my_trips'
         end
